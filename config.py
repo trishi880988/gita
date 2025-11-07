aa682182eed92b907323743f2b3391c6859d6da5
trishi880988 <gauravyt8803@gmail.com>
2025-11-08 04:43:02 +0530

    Remove API credentials from config.py
    Removed sensitive API credentials from config.py

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-# Get these from https://my.telegram.org
-API_ID = 20219694
-API_HASH = "29d9b3a01721ab452fcae79346769e29"
 
-# Get from @BotFather
-BOT_TOKEN = "7942215521:AAG5Zardlr7ULt2-yleqXeKjHKp4AQtVzd8"
